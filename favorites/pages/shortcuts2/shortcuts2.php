65a4aa1c5cc9cece3e43354e9767e976cbfaaf4a
tcarnathan <124714369+tcarnathan@users.noreply.github.com>
2026-06-27 17:09:01 -0400

    Add files via upload

---
+++ favorites/pages/shortcuts2/shortcuts2.php
@@ -0,0 +1,176 @@
+<?php   
+	$pagename = 'shortcuts2';
+	include($pagename."settings.php");
+
+?>
+<div style="width: 2000">
+
+<html><body bgcolor=rgb(0,204,202)>
+
+<style> 
+
+#topp{
+
+			
+			font-size: 70px;
+			color: white;
+			font-weight: bold;
+			text-align: center;
+			text-transform: capitalize;
+			max-width: 530px;
+			width: 100%;
+			margin-right: auto;
+			margin-left: auto;
+			padding-left: 5px;
+			padding-right: 5px;
+			margin-bottom:10px;
+			border: 2px solid white;
+			background-color: #0015BF;
+
+
+</style>
+<br>
+
+<br>
+
+<h1 id=topp> <?php echo $pagename ?> </h1>
+<br>
+<!-- <img src="f.jpg" alt="Gpig" style="width:250px;height:150px;">
+-->
+<br> <!-- Row 1 -->
+
+<!-- Link 1 -->
+<a href="<?php echo $link1 ?>"><img src="<?php echo '../../images/'.$image1 ?>" width="250" height="150" border="0" alt="<?php echo $txt1 ?>" /></a> 
+
+
+<!-- Link 2 -->
+<a href="<?php echo $link2 ?>"><img src="<?php echo '../../images/'.$image2 ?>" width="250" height="150" border="0" alt="<?php echo $txt2 ?>" /></a> 
+
+<!-- Link 3 -->
+<a href="<?php echo $link3 ?>"><img src="<?php echo '../../images/'.$image3 ?>" width="250" height="150" border="0" alt="<?php echo $txt3 ?>" /></a> 
+
+<!-- Link 4 -->
+<a href="<?php echo $link4 ?>"><img src="<?php echo '../../images/'.$image4 ?>" width="250" height="150" border="0" alt="<?php echo $txt4 ?>" /></a> 
+
+<!-- Link 5 -->
+<a href="<?php echo $link5 ?>"><img src="<?php echo '../../images/'.$image5 ?>" width="250" height="150" border="0" alt="<?php echo $txt5 ?>" /></a> 
+
+<!-- Link 6 -->
+<a href="<?php echo $link6 ?>"><img src="<?php echo '../../images/'.$image6 ?>" width="250" height="150" border="0" alt="<?php echo $txt6 ?>" /></a> 
+
+<!-- Link 7 -->
+<a href="<?php echo $link7 ?>"><img src="<?php echo '../../images/'.$image7 ?>" width="250" height="150" border="0" alt="<?php echo $txt7 ?>" /></a> 
+
+
+
+
+<br> <!-- Row 2 -->
+
+
+
+<!-- Link 8 -->
+<a href="<?php echo $link8 ?>"><img src="<?php echo '../../images/'.$image8 ?>" width="250" height="150" border="0" alt="<?php echo $txt8 ?>" /></a> 
+
+<!-- Link 9 -->
+<a href="<?php echo $link9 ?>"><img src="<?php echo '../../images/'.$image9 ?>" width="250" height="150" border="0" alt="<?php echo $txt9 ?>" /></a> 
+
+<!-- Link 10 -->
+<a href="<?php echo $link10 ?>"><img src="<?php echo '../../images/'.$image10 ?>" width="250" height="150" border="0" alt="<?php echo $txt10 ?>" /></a> 
+
+<!-- Link 11 -->
+<a href="<?php echo $link11 ?>"><img src="<?php echo '../../images/'.$image11 ?>" width="250" height="150" border="0" alt="<?php echo $txt11 ?>" /></a>
+
+<!-- Link 12 -->
+<a href="<?php echo $link12 ?>"><img src="<?php echo '../../images/'.$image12 ?>" width="250" height="150" border="0" alt="<?php echo $txt12 ?>" /></a>  
+
+<!-- Link 13 -->
+<a href="<?php echo $link13 ?>"><img src="<?php echo '../../images/'.$image13 ?>" width="250" height="150" border="0" alt="<?php echo $txt13 ?>" /></a> 
+
+<!-- Link 14 -->
+<a href="<?php echo $link14 ?>"><img src="<?php echo '../../images/'.$image14 ?>" width="250" height="150" border="0" alt="<?php echo $txt14 ?>" /></a> 
+
+
+
+<br>  <!-- Row 3 -->
+
+
+<!-- Link 15 -->
+<a href="<?php echo $link15 ?>"><img src="<?php echo '../../images/'.$image15 ?>" width="250" height="150" border="0" alt="<?php echo $txt15 ?>" /></a> 
+
+<!-- Link 16 -->
+<a href="<?php echo $link16 ?>"><img src="<?php echo '../../images/'.$image16 ?>" width="250" height="150" border="0" alt="<?php echo $txt16 ?>" /></a> 
+
+<!-- Link 17 -->
+<a href="<?php echo $link17 ?>"><img src="<?php echo '../../images/'.$image17 ?>" width="250" height="150" border="0" alt="<?php echo $txt17 ?>" /></a> 
+
+<!-- Link 18 -->
+<a href="<?php echo $link18 ?>"><img src="<?php echo '../../images/'.$image18 ?>" width="250" height="150" border="0" alt="<?php echo $txt18 ?>" /></a>
+
+<!-- Link 19 -->
+<a href="<?php echo $link19 ?>"><img src="<?php echo '../../images/'.$image19 ?>" width="250" height="150" border="0" alt="<?php echo $txt19 ?>" /></a>  
+
+<!-- Link 20 -->
+<a href="<?php echo $link20 ?>"><img src="<?php echo '../../images/'.$image20 ?>" width="250" height="150" border="0" alt="<?php echo $txt20 ?>" /></a> 
+
+<!-- Link 21 -->
+<a href="<?php echo $link21 ?>"><img src="<?php echo '../../images/'.$image21 ?>" width="250" height="150" border="0" alt="<?php echo $txt21 ?>" /></a> 
+
+
+
+
+
+<!-- blank -->
+
+<!-- <img src="f.jpg" alt="Gpig" style="width:250px;height:150px;">
+
+-->
+
+<br>  <!-- Row 4 -->
+
+
+<!-- Link 22-->
+<a href="<?php echo $link22?>"><img src="<?php echo '../../images/'.$image22?>" width="250" height="150" border="0" alt="<?php echo $txt22?>" /></a> 
+
+<!-- Link 23 -->
+<a href="<?php echo $link23 ?>"><img src="<?php echo '../../images/'.$image23 ?>" width="250" height="150" border="0" alt="<?php echo $txt23 ?>" /></a> 
+
+<!-- Link 24 -->
+<a href="<?php echo $link24 ?>"><img src="<?php echo '../../images/'.$image24 ?>" width="250" height="150" border="0" alt="<?php echo $txt24 ?>" /></a> 
+
+<!-- Link 25 -->
+<a href="<?php echo $link25 ?>"><img src="<?php echo '../../images/'.$image25 ?>" width="250" height="150" border="0" alt="<?php echo $txt25 ?>" /></a>
+
+<!-- Link 26 -->
+<a href="<?php echo $link26 ?>"><img src="<?php echo '../../images/'.$image26 ?>" width="250" height="150" border="0" alt="<?php echo $txt26 ?>" /></a>  
+
+<!-- Link 27 -->
+<a href="<?php echo $link27 ?>"><img src="<?php echo '../../images/'.$image27 ?>" width="250" height="150" border="0" alt="<?php echo $txt27 ?>" /></a> 
+
+<!-- Link 28 -->
+<a href="<?php echo $link28 ?>"><img src="<?php echo '../../images/'.$image28 ?>" width="250" height="150" border="0" alt="<?php echo $txt28 ?>" /></a> 
+
+
+
+
+
+
+
+
+
+<br><br><br><br><br>
+
+
+
+
+
+
+<!-- Editor  -->
+<a href="../../settings/editor.php?pagename=<?php echo $pagename ?>"><img src="../../images/edit.png" width="250" height="150" border="0" alt="editor" /></a> 
+
+
+
+last update 6/27/2026
+
+</body>
+</html>
+</div> 
---
+++ favorites/settings/setup.php
@@ -0,0 +1,233 @@
+<?php
+
+
+
+?>
+
+	
+<?php
+	$pagename = $_GET['pagename'];
+	$blockNameSpace = $_GET['id'];
+	$linkpointer = $_GET['id'];
+	$imgdir = '../images';
+	$files1 = scandir($imgdir);
+
+	
+
+		
+	
+	if($pagename=="index"){
+
+		$configdir = '../'.$pagename.'config.txt';
+		$settingsdir = '../'.$pagename.'settings.php';
+
+
+		}
+	else{
+
+		$configdir = '../pages/'.$pagename.'/'.$pagename.'config.txt';
+		$settingsdir = '../pages/'.$pagename.'/'.$pagename.'settings.php';
+
+		}	
+	
+	
+	
+	if(file_exists($configdir)){
+
+#		$configfile = file_get_contents('../indexconfig.txt');
+		$configfile = file_get_contents($configdir);
+		$blockInfo = json_decode($configfile,true);
+#		echo($configfile);
+#		echo($blockInfo);
+	}
+	if(file_exists($settingsdir)){
+		include($settingsdir);
+	}
+
+
+	// iterate variables
+	for ($num = 1; $num <= 28; $num++) {
+		foreach($blockInfo['variables']['link'.$num] as $variable){
+			$variableList[] = $variable['variable'];
+			if(!isset(${$variable['variable']})){
+				${$variable['variable']} = $variable['default'];
+			}
+			if(${$variable['variable']}=== true){
+				${$variable['variable']} = "true";
+			}
+			if(${$variable['variable']}=== false){
+				${$variable['variable']} = "false";
+			}
+
+
+	}}	
+	
+
+
+#	echo 'test2';
+?>
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo 'Link Setup'?></title>
+		<style>
+			.inputSet{
+				font-weight: bold;
+				margin: 20px;
+				margin-left: 20px;
+				font-size:20px;
+			}
+			#main2{
+			font-size:30px;
+			color:white;
+			font-weight: bold;
+			text-align: center;
+			max-width: 1500px;
+			width: 100%;
+			margin-right: auto;
+			margin-left: auto;
+			padding-left: 5px;
+			padding-right: 5px;
+			margin-bottom:10px;
+			border: 1px solid white;
+			background-color: #0015BF;
+		}
+		</style>
+	</head>
+	<body bgcolor=rgb(#5F1000)>
+		<div id="main2">
+
+			<div class="textDiv">
+
+				<h1>Settings for <?php echo ($linkpointer)?> on the  Settings <?php echo ($blockInfo['name'])?> page</h1></br>
+
+				<form method="post" action="filesave.php" id='blockSettingsForm'>
+					<div style="text-align:left;font-size:1.2em;font-weight:bold;font-variant:small-caps">
+						Version: <?php echo number_format($blockInfo['version'],1,".","")?>
+					</div>
+					<?php
+						for ($ehh = 1; $ehh <= 28; $ehh++) {
+
+							foreach($blockInfo['variables']['link'.$ehh] as $variable){
+#								echo "<br>";
+
+#								echo $variable['desc']."<br>";
+								$varLength = strlen(${$variable['variable']});
+
+								if($varLength<45){
+									$varLength = 50;
+									$varLength = 50;
+								}
+								if($varLength>120){
+									$varLength = 120;
+								}
+								if(${$variable['variable']}=="true"){
+									echo "<select hidden name='".$variable['variable']."' class='button inputSet'><option value='true' selected>True</option><option value='false'>False</option></select>";
+									
+								}
+								else if(${$variable['variable']}=="false"){
+									echo "<select hidden name='".$variable['variable']."' class='button inputSet'><option value='true'>True</option><option value='false' selected>False</option></select>";
+								}
+
+								
+								else{
+									echo "<input type='hidden' name='".$variable['variable']."' value='".${$variable['variable']}."' class='button2 inputSet' size=".$varLength.">";
+								}
+						}
+							
+
+}
+						foreach($blockInfo['variables'][$linkpointer] as $variable){
+							echo "<br>";
+
+							echo $variable['desc']."<br>";
+							$varLength = strlen(${$variable['variable']});
+
+							if($varLength<45){
+								$varLength = 50;
+								$varLength = 50;
+							}
+							if($varLength>120){
+								$varLength = 120;
+							}
+							if(${$variable['variable']}=="true"){
+								echo "<select name='".$variable['variable']."' class='button inputSet'><option value='true' selected>True</option><option value='false'>False</option></select>";
+							}
+							else if(${$variable['variable']}=="false"){
+								echo "<select name='".$variable['variable']."' class='button inputSet'><option value='true'>True</option><option value='false' selected>False</option></select>";
+							}
+							else if($variable['desc']=="image"){ 
+
+							
+							
+
+#							echo "<select name=".$variable['variable']."'";
+							echo "<select name='".$variable['variable']."' class='button inputSet'>"; ?>
+									<?php foreach ($files1 as $files2): ?>
+										<option value="<?php echo $files2; ?>" <?php echo ($files2 == ${$variable['variable']}) ? 'selected' : '' ?>>
+											<?php echo $files2; ?>
+										</option>
+									<?php endforeach; ?>
+								</select>
+								
+								
+								
+							<?php }
+							
+							else{
+#								echo $variable['desc']."<br>";
+
+#								echo $blockInfo['dropdown'];
+								echo "<input name='".$variable['variable']."' value='".${$variable['variable']}."' class='button2 inputSet' size=".$varLength.">";
+							}
+						}
+
+					?>
+					
+					<input type="hidden" name="id" value='<?php echo $blockNameSpace?>'> 
+					<input type="hidden" name="pagename" value='<?php echo $pagename?>'> 
+					<input type="hidden" name="settingsdir" value='<?php echo $settingsdir?>'> 
+					<input type="hidden" name="parameters" value='<?php echo implode(',',$variableList)?>'>
+					<input type="hidden" name="version" value='<?php echo $blockInfo['version']?>'>
+					<br><br>
+					<div style="width:100%;text-align:center">
+						<input type="submit" value="Save Settings" class="button2">
+					</div>
+					<br><br>
+				</form>
+			</div>
+			
+		</div>
+
+		
+		<center>
+		<br>
+			<table cellpadding="9" border="0" cellspacing="0" style="border:solid 1px #000;background-color:#3cc">
+				<tr><td valign="center">
+				<H1>Image Uploader</h1>
+				<form action="upload.php" method="post" enctype="multipart/form-data">
+				Select image to upload:
+				<input type="file" name="fileToUpload" id="fileToUpload">
+				<input type="submit" value="Upload Image" name="submit">
+				</form>
+	
+			</td></tr>
+	
+		</table>
+		</center>
+
+
+	</body>
+</html>
+
+
