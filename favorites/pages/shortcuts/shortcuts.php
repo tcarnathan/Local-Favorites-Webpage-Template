7c37de7fff719d201362d2e1f50a4d30f84220bd
tcarnathan <124714369+tcarnathan@users.noreply.github.com>
2026-06-28 21:28:04 -0400

    Add files via upload

--- favorites/pages/shortcuts/shortcuts.php
+++ favorites/pages/shortcuts/shortcuts.php
@@ -2,11 +2,31 @@
 	$tileswide = 7; 
 	$pagename = 'shortcuts';
 	include($pagename."settings.php");
-$pagewidth = ($width29 * $tileswide)+100;
+
+if($width29 < 100){
+	$tilewidth = 100;
+}
+else{
+	$tilewidth = $width29;
+}
+
+
+
+
+
+if($height29 < 100){
+	$tileheight = 100;
+}
+else{
+	$tileheight = $height29;
+
+}
+$pagewidth = ($tilewidth * $tileswide)+100;
 
 echo '<div style="width: '.$pagewidth.'">';
 
 ?>
+
 
 
 <html><body bgcolor=<?php echo $color29a?>>
@@ -51,26 +71,26 @@ echo '<div style="width: '.$pagewidth.'">';
 <br> <!-- Row 1 -->
 
 <!-- Link 1 -->
-<a href="<?php echo $link1 ?>"><img src="<?php echo '../../images/'.$image1 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt1 ?>" /></a> 
+<a href="<?php echo $link1 ?>"><img src="<?php echo '../../images/'.$image1 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt1 ?>" /></a> 
 
 
 <!-- Link 2 -->
-<a href="<?php echo $link2 ?>"><img src="<?php echo '../../images/'.$image2 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt2 ?>" /></a> 
+<a href="<?php echo $link2 ?>"><img src="<?php echo '../../images/'.$image2 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt2 ?>" /></a> 
 
 <!-- Link 3 -->
-<a href="<?php echo $link3 ?>"><img src="<?php echo '../../images/'.$image3 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt3 ?>" /></a> 
+<a href="<?php echo $link3 ?>"><img src="<?php echo '../../images/'.$image3 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt3 ?>" /></a> 
 
 <!-- Link 4 -->
-<a href="<?php echo $link4 ?>"><img src="<?php echo '../../images/'.$image4 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt4 ?>" /></a> 
+<a href="<?php echo $link4 ?>"><img src="<?php echo '../../images/'.$image4 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt4 ?>" /></a> 
 
 <!-- Link 5 -->
-<a href="<?php echo $link5 ?>"><img src="<?php echo '../../images/'.$image5 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt5 ?>" /></a> 
+<a href="<?php echo $link5 ?>"><img src="<?php echo '../../images/'.$image5 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt5 ?>" /></a> 
 
 <!-- Link 6 -->
-<a href="<?php echo $link6 ?>"><img src="<?php echo '../../images/'.$image6 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt6 ?>" /></a> 
+<a href="<?php echo $link6 ?>"><img src="<?php echo '../../images/'.$image6 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt6 ?>" /></a> 
 
 <!-- Link 7 -->
-<a href="<?php echo $link7 ?>"><img src="<?php echo '../../images/'.$image7 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt7 ?>" /></a> 
+<a href="<?php echo $link7 ?>"><img src="<?php echo '../../images/'.$image7 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt7 ?>" /></a> 
 
 
 
@@ -80,25 +100,25 @@ echo '<div style="width: '.$pagewidth.'">';
 
 
 <!-- Link 8 -->
-<a href="<?php echo $link8 ?>"><img src="<?php echo '../../images/'.$image8 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt8 ?>" /></a> 
+<a href="<?php echo $link8 ?>"><img src="<?php echo '../../images/'.$image8 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt8 ?>" /></a> 
 
 <!-- Link 9 -->
-<a href="<?php echo $link9 ?>"><img src="<?php echo '../../images/'.$image9 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt9 ?>" /></a> 
+<a href="<?php echo $link9 ?>"><img src="<?php echo '../../images/'.$image9 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt9 ?>" /></a> 
 
 <!-- Link 10 -->
-<a href="<?php echo $link10 ?>"><img src="<?php echo '../../images/'.$image10 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt10 ?>" /></a> 
+<a href="<?php echo $link10 ?>"><img src="<?php echo '../../images/'.$image10 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt10 ?>" /></a> 
 
 <!-- Link 11 -->
-<a href="<?php echo $link11 ?>"><img src="<?php echo '../../images/'.$image11 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt11 ?>" /></a>
+<a href="<?php echo $link11 ?>"><img src="<?php echo '../../images/'.$image11 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt11 ?>" /></a>
 
 <!-- Link 12 -->
-<a href="<?php echo $link12 ?>"><img src="<?php echo '../../images/'.$image12 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt12 ?>" /></a>  
+<a href="<?php echo $link12 ?>"><img src="<?php echo '../../images/'.$image12 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt12 ?>" /></a>  
 
 <!-- Link 13 -->
-<a href="<?php echo $link13 ?>"><img src="<?php echo '../../images/'.$image13 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt13 ?>" /></a> 
+<a href="<?php echo $link13 ?>"><img src="<?php echo '../../images/'.$image13 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt13 ?>" /></a> 
 
 <!-- Link 14 -->
-<a href="<?php echo $link14 ?>"><img src="<?php echo '../../images/'.$image14 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt14 ?>" /></a> 
+<a href="<?php echo $link14 ?>"><img src="<?php echo '../../images/'.$image14 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt14 ?>" /></a> 
 
 
 
@@ -106,25 +126,25 @@ echo '<div style="width: '.$pagewidth.'">';
 
 
 <!-- Link 15 -->
-<a href="<?php echo $link15 ?>"><img src="<?php echo '../../images/'.$image15 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt15 ?>" /></a> 
+<a href="<?php echo $link15 ?>"><img src="<?php echo '../../images/'.$image15 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt15 ?>" /></a> 
 
 <!-- Link 16 -->
-<a href="<?php echo $link16 ?>"><img src="<?php echo '../../images/'.$image16 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt16 ?>" /></a> 
+<a href="<?php echo $link16 ?>"><img src="<?php echo '../../images/'.$image16 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt16 ?>" /></a> 
 
 <!-- Link 17 -->
-<a href="<?php echo $link17 ?>"><img src="<?php echo '../../images/'.$image17 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt17 ?>" /></a> 
+<a href="<?php echo $link17 ?>"><img src="<?php echo '../../images/'.$image17 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt17 ?>" /></a> 
 
 <!-- Link 18 -->
-<a href="<?php echo $link18 ?>"><img src="<?php echo '../../images/'.$image18 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt18 ?>" /></a>
+<a href="<?php echo $link18 ?>"><img src="<?php echo '../../images/'.$image18 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt18 ?>" /></a>
 
 <!-- Link 19 -->
-<a href="<?php echo $link19 ?>"><img src="<?php echo '../../images/'.$image19 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt19 ?>" /></a>  
+<a href="<?php echo $link19 ?>"><img src="<?php echo '../../images/'.$image19 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt19 ?>" /></a>  
 
 <!-- Link 20 -->
-<a href="<?php echo $link20 ?>"><img src="<?php echo '../../images/'.$image20 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt20 ?>" /></a> 
+<a href="<?php echo $link20 ?>"><img src="<?php echo '../../images/'.$image20 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt20 ?>" /></a> 
 
 <!-- Link 21 -->
-<a href="<?php echo $link21 ?>"><img src="<?php echo '../../images/'.$image21 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt21 ?>" /></a> 
+<a href="<?php echo $link21 ?>"><img src="<?php echo '../../images/'.$image21 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt21 ?>" /></a> 
 
 
 
@@ -140,25 +160,25 @@ echo '<div style="width: '.$pagewidth.'">';
 
 
 <!-- Link 22-->
-<a href="<?php echo $link22?>"><img src="<?php echo '../../images/'.$image22?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt22?>" /></a> 
+<a href="<?php echo $link22?>"><img src="<?php echo '../../images/'.$image22?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt22?>" /></a> 
 
 <!-- Link 23 -->
-<a href="<?php echo $link23 ?>"><img src="<?php echo '../../images/'.$image23 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt23 ?>" /></a> 
+<a href="<?php echo $link23 ?>"><img src="<?php echo '../../images/'.$image23 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt23 ?>" /></a> 
 
 <!-- Link 24 -->
-<a href="<?php echo $link24 ?>"><img src="<?php echo '../../images/'.$image24 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt24 ?>" /></a> 
+<a href="<?php echo $link24 ?>"><img src="<?php echo '../../images/'.$image24 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt24 ?>" /></a> 
 
 <!-- Link 25 -->
-<a href="<?php echo $link25 ?>"><img src="<?php echo '../../images/'.$image25 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt25 ?>" /></a>
+<a href="<?php echo $link25 ?>"><img src="<?php echo '../../images/'.$image25 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt25 ?>" /></a>
 
 <!-- Link 26 -->
-<a href="<?php echo $link26 ?>"><img src="<?php echo '../../images/'.$image26 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt26 ?>" /></a>  
+<a href="<?php echo $link26 ?>"><img src="<?php echo '../../images/'.$image26 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt26 ?>" /></a>  
 
 <!-- Link 27 -->
-<a href="<?php echo $link27 ?>"><img src="<?php echo '../../images/'.$image27 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt27 ?>" /></a> 
+<a href="<?php echo $link27 ?>"><img src="<?php echo '../../images/'.$image27 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt27 ?>" /></a> 
 
 <!-- Link 28 -->
-<a href="<?php echo $link28 ?>"><img src="<?php echo '../../images/'.$image28 ?>" width="<?php echo $width29 ?>" height="<?php echo $height29 ?>" border="0" alt="<?php echo $txt28 ?>" /></a> 
+<a href="<?php echo $link28 ?>"><img src="<?php echo '../../images/'.$image28 ?>" width="<?php echo $tilewidth ?>" height="<?php echo $tileheight ?>" border="0" alt="<?php echo $txt28 ?>" /></a> 
 
 
 
